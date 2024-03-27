c553a29295cb64dba40aad5b9d0d168ecf3dece0
tiago <tiago.duarte@portalsesisp.org.br>
2024-03-27 17:07:55 -0300

    atividade 25 exercicos

---
+++ Atividade/js/script.js
@@ -0,0 +1,14 @@
+//   EXERCICIO 1 ********
+// const exercicio1 = () => {
+//   for (let i = 1; i <= 10; i++) {
+//     document.getElementById("resposta").innerHTML += `#${i}`;
+//   }
+// };
+//   EXERCICIO 2 ********
+const exercicio2 = () => {
+let num =  document.getElementById("num")
+  for (let i = 0; i <= num ; i += 2) {
+    document.getElementById("resposta").innerHTML += `<h2>${i}</h2>`
+  }
+};
+//   EXERCICIO 3 ********
